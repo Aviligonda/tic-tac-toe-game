f6cb7bc667fc39502f100d3d161cdddafa3cfa46
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-06 19:56:56 +0530

    Added : Player choose x or o

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public static char[] gameBoard = new char[10];
+    public static char userTurn;
+    public static char computerTurn;
+    public static char user = '1';
+    public static Scanner scanner = new Scanner(System.in);
 
 
     public static void showBoard() {
@@ -10,11 +16,16 @@ public class TicTacToe {
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 
+    public static char user() {
+        System.out.println("Enter your choice  X or O only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game");
         showBoard();
-
+        user();
 
     }
 }
